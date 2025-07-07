a467abcd38255086bef1777e3e25aa9d8e5d638f
Abdul Rafay <130937186+M-Abdul-RAFAY@users.noreply.github.com>
2025-07-08 01:09:06 +0500

    Skip ESLint during Vercel build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ This skips ESLint checks during `next build`
+  },
 };
 
 export default nextConfig;
