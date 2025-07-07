d10dad3b9a54c107eb3b92bbe49c255d346e2c60
Abdul Rafay <130937186+M-Abdul-RAFAY@users.noreply.github.com>
2025-07-08 01:14:36 +0500

    Disable TypeScript build errors for Vercel

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   eslint: {
-    ignoreDuringBuilds: true, // ✅ This skips ESLint checks during `next build`
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true, // ✅ This skips TypeScript build errors
   },
 };
 
