17df66a42d30d02674564ac22e03edbd402334e3
Abdul Rafay <130937186+M-Abdul-RAFAY@users.noreply.github.com>
2025-07-09 19:19:19 +0500

    Refactor layout: Integrate ClerkProvider and UserSyncProvider into RootLayout

--- app/(dynamic)/layout.tsx
+++ app/(dynamic)/layout.tsx
@@ -1,3 +1,5 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import UserSyncProvider from "@/components/UserSyncProvider";
 import TopNavigation from "@/components/TopNavigation";
 
 export default function RootLayout({
@@ -6,15 +8,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased h-screen flex flex-col overflow-hidden">
-        <div className="flex-shrink-0 fixed top-0 left-0 right-0 z-50">
-          <TopNavigation />
-        </div>
-        <main className="flex-1 min-h-0 flex flex-col  mt-[3.8rem]">
-          {children}
-        </main>
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        variables: {
+          colorPrimary: "blue",
+          colorText: "black",
+        },
+      }}
+    >
+      <html lang="en">
+        <body className="antialiased h-screen flex flex-col overflow-hidden">
+          <UserSyncProvider>
+            <div className="flex-shrink-0 fixed top-0 left-0 right-0 z-50">
+              <TopNavigation />
+            </div>
+            <main className="flex-1 min-h-0 flex flex-col mt-[3.8rem]">
+              {children}
+            </main>
+          </UserSyncProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
