de1df35d77f652e67c98b1914058a37f32d71bdb
Abdul Rafay <130937186+M-Abdul-RAFAY@users.noreply.github.com>
2025-07-25 00:25:05 +0500

    refactor: comment out MainDashboard import and return statement in DashboardPage component

--- app/(dynamic)/dashboard/page.tsx
+++ app/(dynamic)/dashboard/page.tsx
@@ -1,5 +1,9 @@
-import MainDashboard from "@/components/MainDashboard";
+// import MainDashboard from "@/components/MainDashboard";
+
+import { redirect } from "next/navigation";
 
 export default function DashboardPage() {
-  return <MainDashboard />;
+  // return <MainDashboard />;
+
+  redirect("/dashboard/inbox");
 }
