75fbf1cb6804e891779a6dc344fa86829710f403
M-Abdul-Rafay <muhammadabdulrafay1@gmail.com>
2025-09-11 23:02:20 +0500

    Wrap MessagingApp component in a div for consistent layout and overflow handling

--- app/(dynamic)/dashboard/social-media/page.tsx
+++ app/(dynamic)/dashboard/social-media/page.tsx
@@ -1,7 +1,11 @@
 import MessagingApp from "@/components/messagingApp";
 
 const page = () => {
-  return <MessagingApp />;
+  return (
+    <div className="w-full min-h-full overflow-hidden">
+      <MessagingApp />
+    </div>
+  );
 };
 
 export default page;
